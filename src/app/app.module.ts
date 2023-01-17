5bf5e7556f451290c435ba45c5d045ba1a9c58e6
Dioonel <dionelmartinezc@gmail.com>
2023-01-17 15:37:35 -0300

    Finished kanji-search base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +25,8 @@ import { KanjiSearchComponent } from './components/kanji-search/kanji-search.com
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/kanji-search/kanji-search.component.ts
+++ src/app/components/kanji-search/kanji-search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { NgModel, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-kanji-search',
@@ -6,10 +8,112 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./kanji-search.component.css']
 })
 export class KanjiSearchComponent implements OnInit {
+  filterType: string = '0';
+  filterKanji: FormControl = new FormControl('', [Validators.required, Validators.minLength(1), Validators.pattern(/[\u4e00-\u9faf]/)]);
+  filterPronunciation: FormControl = new FormControl('', [Validators.required, Validators.minLength(1), Validators.pattern(/[\u3040-\u30ff]/)]);
+  filterMeaning: FormControl = new FormControl('', [Validators.required, Validators.minLength(1)]);
+  filterCategory: FormControl = new FormControl('', Validators.required);
+  isBack = false;
+  isNext = false;
 
-  constructor() { }
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
   }
 
+  offset(n: number){
+    this.activatedRoute.queryParams.subscribe(params => {
+      let myQuery: Params = {};
+      if(params['title']) {
+        myQuery['title'] = params['title'];
+      }
+      myQuery['offset'] = n;
+      this.router.navigate([], {relativeTo: this.activatedRoute, queryParams: myQuery, queryParamsHandling: 'merge'});
+      window.location.reload();
+    });
+  }
+
+  paginateConfig(n: number){
+    this.activatedRoute.queryParams.subscribe(params => {
+      if(params['offset']) {
+        if(params['offset'] > 0) {
+          this.isBack = true;
+        }
+        if(params['offset'] < n) {
+          this.isNext = true;
+        }
+      } else {
+        if(n > 0) {
+          this.isNext = true;
+        }
+      }
+    });
+  }
+
+  backO(){
+    this.activatedRoute.queryParams.subscribe(params => {
+      if(params['offset']) {
+        this.offset(params['offset'] - 1);
+      }
+    });
+  }
+
+  next(){
+    this.activatedRoute.queryParams.subscribe(params => {
+      if(params['offset']) {
+        this.offset(Number(params['offset']) + 1);
+      } else {
+        this.offset(1);
+      }
+    });
+  }
+
+  filter() {
+    console.log(this.filterType);
+    if(this.filterType === '0'){
+      console.log('refresh loco XD');
+    } else if(this.filterType === '1'){
+      if(this.filterKanji.valid) {
+        console.log(this.filterKanji.value);
+      }
+    } else if(this.filterType === '2'){
+      if(this.filterPronunciation.valid) {
+        console.log(this.filterPronunciation.value);
+      }
+    } else if(this.filterType === '3'){
+      if(this.filterMeaning.valid) {
+        console.log(this.filterMeaning.value);
+      }
+    } else if(this.filterType === '4'){
+      if(this.filterCategory.valid) {
+        console.log(this.filterCategory.value);
+      }
+    } else {
+      console.log('???');
+    }
+  }
+
+  resetPrevious() {
+    console.log('reset?');
+    this.filterKanji.reset();
+    this.filterPronunciation.reset();
+    this.filterMeaning.reset();
+    this.filterCategory.reset();
+  }
+
+  markAsTouched() {
+    if(this.filterType === '1'){
+      this.filterKanji.markAsDirty();
+    } else if(this.filterType === '2'){
+      this.filterPronunciation.markAsDirty();
+    } else if(this.filterType === '3'){
+      this.filterMeaning.markAsDirty();
+    } else if(this.filterType === '4'){
+      this.filterCategory.markAsDirty();
+    }
+  }
+
+  back(){
+    window.history.back();
+  }
 }
