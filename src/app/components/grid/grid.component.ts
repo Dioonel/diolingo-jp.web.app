b6557a82cb4c80cc7b567fc3a005534bb83d55aa
Dioonel <dionelmartinezc@gmail.com>
2023-05-22 15:31:53 -0300

    Added a 'shuffle' button to randomize the order of the elements in grid component

--- src/app/components/grid/grid.component.ts
+++ src/app/components/grid/grid.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
-import { faPencil, faA, faAsterisk, faWandMagicSparkles } from '@fortawesome/free-solid-svg-icons';
+import { faPencil, faA, faAsterisk, faWandMagicSparkles, faDiceSix } from '@fortawesome/free-solid-svg-icons';
 import { Dialog } from '@angular/cdk/dialog';
 import { DialogComponent } from '../dialog/dialog.component';
 
@@ -17,6 +17,7 @@ export class GridComponent implements OnInit {
   faA = faA;
   faAsterisk = faAsterisk;
   faWandMagicSparkles = faWandMagicSparkles;
+  faDiceSix = faDiceSix;
   switch!: 'japanese' | 'meaning' | 'all';
 
   constructor(private router: Router, private dialog: Dialog) {
@@ -48,5 +49,12 @@ export class GridComponent implements OnInit {
   doSwitch(type: 'japanese' | 'meaning' | 'all') {
     this.switch = type;
     sessionStorage.setItem('switch', type);
+  }
+
+  randomize() {
+    for (let i = this.items.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.items[i], this.items[j]] = [this.items[j], this.items[i]];
+    }
   }
 }
