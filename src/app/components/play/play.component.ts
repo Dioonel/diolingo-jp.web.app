14153a828bc8ca0dc2101feebb3da4d86d386c3a
Dioonel <dionelmartinezc@gmail.com>
2023-06-22 15:29:22 -0300

    Optimized images

--- src/app/components/play/play.component.ts
+++ src/app/components/play/play.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./play.component.css']
 })
 export class PlayComponent implements OnInit {
+  loaded = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.loaded = true;
   }
 
 }
