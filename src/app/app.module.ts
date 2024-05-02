5432e812a12462ecb8ffb6d9e808b034c2ab7e37
Dioonel <dionelmartinezc@gmail.com>
2024-05-02 16:09:36 -0300

    Migrated Material components to MDC

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,8 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { DialogModule } from '@angular/cdk/dialog';
 import { CdkAccordionModule } from '@angular/cdk/accordion';
-import { MatLegacyProgressSpinnerModule as MatProgressSpinnerModule } from '@angular/material/legacy-progress-spinner';
-import { MatLegacyTooltipModule as MatTooltipModule } from '@angular/material/legacy-tooltip';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
--- src/app/components/grid/grid.component.spec.ts
+++ src/app/components/grid/grid.component.spec.ts
@@ -5,7 +5,7 @@ import { SearchComponent } from '../search/search.component';
 import { DialogModule } from '@angular/cdk/dialog';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatLegacyTooltipModule as MatTooltipModule } from '@angular/material/legacy-tooltip';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { queryAll, clickEvent } from './../../../testing/helpers';
 
