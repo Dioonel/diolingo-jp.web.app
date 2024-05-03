5b426ea93b072036d563e956174a151627cfb0a2
Dioonel <dionelmartinezc@gmail.com>
2024-05-03 17:22:09 -0300

    Migration to standalone app finished

---
+++ src/app/app.config.ts
@@ -0,0 +1,14 @@
+import { ApplicationConfig } from "@angular/core";
+import { provideRouter } from '@angular/router';
+import { HTTP_INTERCEPTORS, withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+
+import { routes } from './app.routes';
+import { MyInterceptor } from './interceptors/interceptor.interceptor';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    { provide: HTTP_INTERCEPTORS, useClass: MyInterceptor, multi: true },
+    provideHttpClient(withInterceptorsFromDi())
+  ]
+}
--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,13 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { enableProdMode } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
 
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { CdkAccordionModule } from '@angular/cdk/accordion';
-import { DialogModule } from '@angular/cdk/dialog';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app/app-routing.module';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { MyInterceptor } from './app/interceptors/interceptor.interceptor';
-import { HTTP_INTERCEPTORS, withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule, FontAwesomeModule, DialogModule, CdkAccordionModule, MatProgressSpinnerModule, MatTooltipModule),
-        {
-            provide: HTTP_INTERCEPTORS, useClass: MyInterceptor, multi: true
-        },
-        provideHttpClient(withInterceptorsFromDi())
-    ]
-})
+bootstrapApplication(AppComponent, appConfig)
   .catch(err => console.error(err));
