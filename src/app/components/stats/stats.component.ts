b010ade761502ca49d831121b6e8c45e8883a9c9
Dioonel <dionelmartinezc@gmail.com>
2024-05-07 16:24:20 -0300

    Installed Chart.js and created basic template for Overall stats tab

--- src/app/components/stats/stats.component.ts
+++ src/app/components/stats/stats.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
+import { Chart, ChartItem, registerables } from 'chart.js';
+Chart.register(...registerables);
+
 
 import { MockOverall, MockGuess, MockPairs } from './../../models/stats';
 
@@ -10,6 +13,65 @@ import { MockOverall, MockGuess, MockPairs } from './../../models/stats';
   templateUrl: './stats.component.html',
   styleUrl: './stats.component.css'
 })
-export class StatsComponent {
+export class StatsComponent implements OnInit, AfterViewInit {
+  constructor() {}
 
+  ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit() {
+    this.renderOverall();
+    // this.renderGuess();
+    // this.renderPairs();
+  }
+
+  renderOverall() {
+    const legendMargin: any = {
+      id: 'legendMargin',
+      beforeInit: function(chart: any, legend: any, options: any) {
+        console.log(chart.legend.fit);
+      }
+    };
+
+    new Chart('overall', {
+      type: 'pie',
+      data: {
+        labels: ['Correct', 'Incorrect'],
+        datasets: [{
+          label: '# Answers',
+          data: [MockOverall.total_correct, MockOverall.total_incorrect],
+          backgroundColor: [
+            '#63AAE3',
+            '#E15554'
+          ],
+          borderColor: '',
+          borderWidth: 1
+        }]
+      },
+      options: {
+        plugins: {
+          legend: {
+            labels: {
+              padding: 20,
+              font: {
+                size: 16
+              },
+              color: '#fff',
+            },
+            // Disables legend interaction
+            onClick: (e: any) => e.stopPropagation()
+          },
+          tooltip: {
+            bodyFont: {
+              size: 15
+            },
+            titleFont: {
+              size: 15
+            }
+          }
+        },
+      }
+    });
+  }
 }
